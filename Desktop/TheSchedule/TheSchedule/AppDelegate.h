82db7fe53836aba0ede0234a4f0871dcd32c9098
Sjostrand Gereholt Amelie <sjostrandgereholtamelie@Sjostrand-Gereholt-Amelies-MacBook-Pro.local>
2013-10-12 16:01:31 +0200

    first commit

---
+++ Desktop/TheSchedule/TheSchedule/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheSchedule
+//
+//  Created by Sjostrand Gereholt Amelie on 11/10/2013.
+//  Copyright (c) 2013 Amelie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Desktop/TheSchedule/TheScheduleTests/TheScheduleTests.m
@@ -0,0 +1,34 @@
+//
+//  TheScheduleTests.m
+//  TheScheduleTests
+//
+//  Created by Sjostrand Gereholt Amelie on 11/10/2013.
+//  Copyright (c) 2013 Amelie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TheScheduleTests : XCTestCase
+
+@end
+
+@implementation TheScheduleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
